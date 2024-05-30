a586576d56050b0dbf057e9ab71c1e6819d01b5e
karbal abdelhadi <abdoraja2005@gmail.com>
2024-05-30 13:43:16 +0100

    task 5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""script to encrypt a password"""
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """hash a password """
+
+     # Generate a salt
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed_password
