fd4131afcb9e5ead5a75b5d5aa839e01ae30c3e3
karbal abdelhadi <abdoraja2005@gmail.com>
2024-06-03 22:11:47 +0100

    task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,20 +6,23 @@ from typing import List, TypeVar
 from flask import request
 
 
-
 class Auth:
     """ class to manage the API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ doc doc doc string """
-        return False
-    
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """ doc doc doc string """
         if request is None:
             return None
         return request.headers.get("Authorization", None)
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ doc doc doc string """
