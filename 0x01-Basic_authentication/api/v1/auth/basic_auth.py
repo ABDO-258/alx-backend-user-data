91655c32f897a3d2fad4b5d423b8efe8cfe64ec8
karbal abdelhadi <abdoraja2005@gmail.com>
2024-06-04 01:27:01 +0100

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 module to manage the API authentication.
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,18 @@ class BasicAuth(Auth):
             return None
         base64 = authorization_header.split(' ', 1)[1]
         return base64
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode with base64"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            encoding = base64_authorization_header.encode('utf-8')
+            decode_by_64 = b64decode(encoding)
+            decoding = decode_by_64.decode('utf-8')
+        except BaseException:
+            return None
+        return decoding
