5f96661b36dd90169598a2c56f61f7dd5fe8a78f
karbal abdelhadi <abdoraja2005@gmail.com>
2024-06-06 00:11:28 +0100

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+module to manage the API authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ class to manage the API authentication """
+    pass
