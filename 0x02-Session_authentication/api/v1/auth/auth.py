e514be466995dce7ccb231e615ec0f2fb3259e2a
karbal abdelhadi <abdoraja2005@gmail.com>
2024-06-06 00:43:51 +0100

    task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ module to manage the API authentication.
 from typing import List, TypeVar
 from flask import request
 import fnmatch
+import os
 
 
 class Auth:
@@ -33,3 +34,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ doc doc doc string """
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        cookies_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(cookies_name)
